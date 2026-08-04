c9961005514e20c46076f5aefbfd2fb8cd445ab5
Lightingbolt2469 <vihaancool23@gmail.com>
2026-08-04 18:42:15 -0400

    Refactor DVD movement and initialization logic
    Updated the Move function to ensure the DVD texture stays within window bounds and adjusted initial position and dimensions based on texture size.

--- main.cpp
+++ main.cpp
@@ -3,38 +3,62 @@
 
 using namespace std;
 
-void Move(int &x, int &y, int &speedx,int &speedy, int texture_w, int texture_h, int window_w, int window_h) {
+void Move(int &x, int &y, int &speedx, int &speedy, int texture_w, int texture_h, int window_w, int window_h) {
     x += speedx;
     y += speedy;
-    if (x <= 0 || x + texture_w >= window_w) {
+
+    if (x <= 0) {
+        x = 0;
+        speedx *= -1;
+    } 
+    else if (x + texture_w >= window_w) {
+        x = window_w - texture_w;
         speedx *= -1;
     }
-    if (y <= 0 || y + texture_h >= window_h) {
+
+    if (y <= 0) {
+        y = 0;
+        speedy *= -1;
+    } 
+    else if (y + texture_h >= window_h) {
+        y = window_h - texture_h;
         speedy *= -1;
     }
 }
 
 int main() {
     SetTraceLogLevel(LOG_NONE);
-    cout << endl <<  "Starting the simulation...\n";
-    int dvd_x = 600;
-    int dvd_y = 400;
+    cout << endl << "Starting the simulation...\n";
+
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(1200, 800, "DVD nostalgia");
+    SetTargetFPS(60);
+
+    Texture2D dvd = LoadTexture("Images/dvd.png");
+
+    int dvd_width = (dvd.width > 0) ? dvd.width : 230;
+    int dvd_height = (dvd.height > 0) ? dvd.height : 110;
+
+    int dvd_x = 100;
+    int dvd_y = 100;
     int dvd_speedx = 4;
     int dvd_speedy = 4;
-    int dvd_width = 230;
-    int dvd_height = 110;
-    InitWindow(1200, 800, "DVD nostalgia");
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    SetTargetFPS(60);
-    Texture2D dvd = LoadTexture("Images/dvd.png");
-    while (WindowShouldClose() == false) {
+
+    while (!WindowShouldClose()) {
+        int current_w = GetScreenWidth();
+        int current_h = GetScreenHeight();
+
+        Move(dvd_x, dvd_y, dvd_speedx, dvd_speedy, dvd_width, dvd_height, current_w, current_h);
+
         BeginDrawing();
-        ClearBackground(BLACK);
-        Move(dvd_x, dvd_y, dvd_speedx, dvd_speedy, dvd_width, dvd_height, GetScreenWidth(), GetScreenHeight());
-        DrawTexture(dvd , dvd_x, dvd_y, WHITE);
+            ClearBackground(BLACK);
+            DrawTexture(dvd, dvd_x, dvd_y, WHITE);
         EndDrawing();
     }
-    cout << endl <<  "Closing the simulation...\n" << endl;
+
+    cout << endl << "Closing the simulation...\n" << endl;
     UnloadTexture(dvd);
     CloseWindow();
+
+    return 0;
 }
